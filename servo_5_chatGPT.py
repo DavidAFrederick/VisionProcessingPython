5fbd982964d9a7f0fa0eee2c33d9fcb3838d59f0
David Frederick <davidafrederick.va.usa.earth@gmail.com>
2025-04-23 14:24:18 -0400

    working PWM, SW, LED

--- servo_5_chatGPT.py
+++ servo_5_chatGPT.py
@@ -9,8 +9,8 @@ from gpiod.line import Direction, Value
 # Constants
 CHIP = "/dev/gpiochip0"
 PWM_LINE_OFFSET = 15 # 18 is busy  # GPIO pin number (BCM numbering, e.g., GPIO18)
-SW_LINE_OFFSET = 14
-LED_LINE_OFFSET = 27
+SW_LINE_OFFSET = 27
+LED_LINE_OFFSET = 14
 
 PERIOD = 0.02  # 20ms period typical for servo
 MIN_PULSE = 0.0005  # 0.5 ms (0 degrees)
@@ -24,18 +24,23 @@ MAX_PULSE = 0.0025  # 2.5 ms (180 degrees)
 def angle_to_pulse(angle):
     return MIN_PULSE + (angle / 180.0) * (MAX_PULSE - MIN_PULSE)
 
+def gpio_value_to_boolean(input_value) -> bool:
+    if (input_value == Value.ACTIVE):
+        return True
+    return False
+
 # Open chip and request the GPIO line
 chip = gpiod.Chip(CHIP)
 
 
-line = chip.get_line_info(PWM_LINE_OFFSET)
-# line = chip.get_line(PWM_LINE_OFFSET)
+pwm_line = chip.get_line_info(PWM_LINE_OFFSET)
 
 value_str = {Value.ACTIVE: "Active", Value.INACTIVE: "Inactive"}
 value = Value.ACTIVE
 
-# line.request(consumer="servo", type=gpiod.LINE_REQ_DIR_OUT)
-line = gpiod.request_lines( CHIP,  config={PWM_LINE_OFFSET: gpiod.LineSettings(direction=Direction.OUTPUT, output_value=value)})
+pwm_line    = gpiod.request_lines( CHIP,  config={PWM_LINE_OFFSET: gpiod.LineSettings(direction=Direction.OUTPUT, output_value=value)})
+LED_line    = gpiod.request_lines( CHIP,  config={LED_LINE_OFFSET: gpiod.LineSettings(direction=Direction.OUTPUT, output_value=value)})
+switch_line = gpiod.request_lines( CHIP,  config={SW_LINE_OFFSET:  gpiod.LineSettings(direction=Direction.INPUT)})
 
 sleep_time = 0.1
 angle = 0
@@ -61,7 +66,19 @@ def print_header():
 
 try:
     while True:
-        print ("Pin to be used: ", PWM_LINE_OFFSET)
+        
+        # Read the switch status
+        switch_status = switch_line.get_value(SW_LINE_OFFSET)
+        boolean_switch_status = gpio_value_to_boolean (switch_status)
+        print ("switch_status: ", boolean_switch_status)
+
+        # Set the LED to match the switch
+        if (boolean_switch_status):
+            LED_line.set_value(LED_LINE_OFFSET,Value.ACTIVE)
+        else:
+            LED_line.set_value(LED_LINE_OFFSET,Value.INACTIVE)
+
+
         print_header()
         listen_keyboard(on_press = read_key_press,)
 
@@ -89,12 +106,10 @@ try:
         for counter in range(10):
 
             # Generate a single PWM pulse
-            # line.set_value(1)
-            line.set_value(PWM_LINE_OFFSET,Value.ACTIVE)
+            pwm_line.set_value(PWM_LINE_OFFSET,Value.ACTIVE)
             time.sleep(pulse_width)
 
-            # line.set_value(0)
-            line.set_value(PWM_LINE_OFFSET,Value.INACTIVE)
+            pwm_line.set_value(PWM_LINE_OFFSET,Value.INACTIVE)
             time.sleep(PERIOD - pulse_width)
 
         time.sleep(sleep_time)
@@ -103,6 +118,11 @@ except KeyboardInterrupt:
     print("\nExiting...")
 
 finally:
-    # line.set_value(0)
-    line.set_value(PWM_LINE_OFFSET,Value.INACTIVE)
-    line.release()
+    pwm_line.set_value(PWM_LINE_OFFSET,Value.INACTIVE)
+    pwm_line.release()
+
+    LED_line.set_value(LED_LINE_OFFSET,Value.INACTIVE)
+    LED_line.release()
+
+    switch_line.release()
+
