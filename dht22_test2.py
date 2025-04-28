cf3cf6b58b5e3990033995d41a2b5e4ccdf0b2a0
David Frederick <davidafrederick.va.usa.earth@gmail.com>
2025-04-28 16:52:45 -0400

    working on dht 3

--- dht22_test2.py
+++ dht22_test2.py
@@ -1,5 +1,3 @@
-# import RPi.GPIO as GPIO
-# import time
 
 import gpiod
 import time
@@ -7,13 +5,8 @@ from gpiod.line import Direction, Value
 # https://libgpiod.readthedocs.io/en/latest/python_api.html
 
 
-# DHT_PIN = 4  # GPIO pin connected to DHT22 data pin
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(DHT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
 # Constants
 CHIP = "/dev/gpiochip0"
-
 DHT_PIN = 4  # GPIO pin connected to DHT22 data pin
 
 # Initialize the GPIO chip and set a line for output 
@@ -25,132 +18,27 @@ dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN:  gpiod.LineSettings(dire
 time.sleep(0.001)
 dht_line.release()
 time.sleep(1.0)
-print ("aa")
-# # dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN: gpiod.LineSettings(direction=Direction.OUTPUT,output_value=value)})
-
-# dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN: gpiod.LineSettings(direction=Direction.OUTPUT,output_value=Value.ACTIVE)})
-# print ("Output - Active = 1.6V")
-# input("waiting")
-# dht_line.release()
-# time.sleep(0.01)
-
-# dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN: gpiod.LineSettings(direction=Direction.OUTPUT,output_value=Value.INACTIVE)})
-# print ("Output - INActive = 0.0V")
-# input("waiting")
-# dht_line.release()
-# time.sleep(0.01)
-
-# dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN:  gpiod.LineSettings(direction=Direction.INPUT, bias=gpiod.line.Bias.PULL_UP)})
-# print ("Input - Pull UP - 1.1V")
-# input("waiting")
-# dht_line.release()
-# time.sleep(0.01)
-
-# dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN:  gpiod.LineSettings(direction=Direction.INPUT, bias=gpiod.line.Bias.PULL_DOWN)})
-# print ("Input - Pull Down - 1.09V")
-# input("waiting")
-# dht_line.release()
-# time.sleep(0.01)
-
-# dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN:  gpiod.LineSettings(direction=Direction.INPUT)})
-# print ("Input - No Bias - 1.1V")
-# input("waiting")
-
-# dht_line.release()
-# time.sleep(0.01)
-# print ("Released  1.08V")
-# input("waiting")
-
-# for counter in range(20):
-#     # time.sleep(0.001)
-#     # dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN: gpiod.LineSettings(direction=Direction.OUTPUT,output_value=value)})
-#     time.sleep(0.001)
-#     dht_line.set_value(DHT_PIN,Value.ACTIVE)
-#     print ("High")
-#     time.sleep(3.0)
-#     time.sleep(0.001)
-#     dht_line.set_value(DHT_PIN,Value.INACTIVE)
-#     print ("low")
-#     print ("Counter: ", counter)
-#     time.sleep(3.0)
-
-# # dht_line.release()
-# print ("initiaized  2")
-# chip = gpiod.Chip(CHIP)
-
-
-# # dht_line.release()
-
-# dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN: gpiod.LineSettings(direction=Direction.OUTPUT,output_value=Value.INACTIVE)})
-# time.sleep(0.01)
-# dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN: gpiod.LineSettings(direction=Direction.OUTPUT,output_value=Value.ACTIVE)})
-# dht_line.release()
-# time.sleep(0.0001)
-
-
-# # dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN:  gpiod.LineSettings(direction=Direction.INPUT, bias=gpiod.line.Bias.PULL_UP)})
-# dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN:  gpiod.LineSettings(direction=Direction.INPUT)})
-
-# for index in range(5000):
-#     print ( int(dht_line.get_value(DHT_PIN) == Value.INACTIVE), end="" )
-#     time.sleep(0.00001)
-
-
-# dht_line.release()
-# print ("")
-# print ("=================================")
-
-##
-def initialize_chip() -> None:
-    chip = gpiod.Chip(CHIP)
-    value_str = {Value.ACTIVE: "Active", Value.INACTIVE: "Inactive"}
-    value = Value.ACTIVE
-##
-def initialize_input_pin() -> None:        
-    print ("Initializing inputs - ", end=" ")
-    dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN:  gpiod.LineSettings(direction=Direction.INPUT, bias=gpiod.line.Bias.PULL_UP)})
-    print ("Done")
-
-def initialize_output_pin() -> None:        
-    print ("Initializing outputs - ", end=" ")
-    dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN:  gpiod.LineSettings(direction=Direction.INPUT, bias=gpiod.line.Bias.PULL_UP)})
-    print ("Done")
-
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def read_dht22_data():
-    # data = []
-    # time.sleep(1)
-    # GPIO.setup(DHT_PIN, GPIO.OUT)
-    # GPIO.output(DHT_PIN, GPIO.LOW)
-    # time.sleep(0.018)
-    # GPIO.output(DHT_PIN, GPIO.HIGH)
-    # GPIO.setup(DHT_PIN, GPIO.IN)
 
     # pulse the DHT22 pin  go low then high
+    CHIP = "/dev/gpiochip0"
     chip = gpiod.Chip(CHIP)
     value_str = {Value.ACTIVE: "Active", Value.INACTIVE: "Inactive"}
     value = Value.ACTIVE
 
-    print("1a")
     data = []
-    print ("1b")
-    # dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN: gpiod.LineSettings(direction=Direction.OUTPUT, output_value=Value.ACTIVE)})
     dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN: gpiod.LineSettings(direction=Direction.OUTPUT,output_value=value)})
-    #$seline = gpiod.request_lines( CHIP,  config={PWM_LINE_OFFSET: gpiod.LineSettings(direction=Direction.OUTPUT, output_value=self.value)})
 
-    print("1c")
  
     time.sleep(1)
     dht_line.set_value(DHT_PIN,Value.INACTIVE)
     time.sleep(0.018)
-    # input ("waiting - Low")
     dht_line.set_value(DHT_PIN,Value.ACTIVE)
-    # input ("waiting - High")
 
     dht_line.release()
-    # time.sleep(0.018)
     dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN:  gpiod.LineSettings(direction=Direction.INPUT, bias=gpiod.line.Bias.PULL_UP)})
 
     start_time_1 = time.time()
@@ -162,43 +50,28 @@ def read_dht22_data():
     print (dht_line.get_value(DHT_PIN))
     print (dht_line.get_value(DHT_PIN))
 
-    start_time_2 = time.time()
+    # start_time_2 = time.time()
 
     for index in range(500):
         print ( int(dht_line.get_value(DHT_PIN) == Value.INACTIVE), end="" )
-        # time.sleep(0.0001)
 
-    end_time = time.time()
     print("")
-    print ("time to print = ", start_time_2 - start_time_1)
-    print ("time for  500 reads = ", end_time - start_time_2)
-    print ("time for  500 reads = ", (end_time - start_time_2)/500)
-    print ("2")
+
 
     time_now = time.time()
 
-    print ("3")
     # Reconfigure as input
     dht_line.release()
-    print ("3a")
     dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN:  gpiod.LineSettings(direction=Direction.INPUT, bias=gpiod.line.Bias.PULL_UP)})
-    print ("4")
-
-    line_status =  bool(dht_line.get_value(DHT_PIN)) 
-    print ("line_status: ", line_status)
 
     while True:
-        print ("5")
         current_time = time.time()
         if current_time - time_now > 0.0002:
             break
-        print ("6")
-        # if GPIO.input(DHT_PIN) == 0:
         if (bool(dht_line.get_value(DHT_PIN)) == False):
             data.append(0)
         else:
             data.append(1)
-        print ("7")
     
     start = 0
     
@@ -250,9 +123,7 @@ def cleanup_GPIO():
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 try:
-    print ("1")
     humidity, temperature = read_dht22_data()
-    print ("100")
     if humidity is not None and temperature is not None:
         print(f"Temperature: {temperature:.1f}°C, Humidity: {humidity:.1f}%")
     else:
