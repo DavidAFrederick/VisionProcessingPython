cf75df7dde0f760e36a54850b197052a3341c0f9
David Frederick <davidafrederick.va.usa.earth@gmail.com>
2025-04-28 17:59:33 -0400

    working on dht 4

--- dht22_test2.py
+++ dht22_test2.py
@@ -8,14 +8,12 @@ from gpiod.line import Direction, Value
 # Constants
 CHIP = "/dev/gpiochip0"
 DHT_PIN = 4  # GPIO pin connected to DHT22 data pin
+value_str = {Value.ACTIVE: "Active", Value.INACTIVE: "Inactive"}
+value = Value.ACTIVE
 
 # Initialize the GPIO chip and set a line for output 
 chip = gpiod.Chip(CHIP)
-value_str = {Value.ACTIVE: "Active", Value.INACTIVE: "Inactive"}
-value = Value.ACTIVE
-time.sleep(0.001)
-dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN:  gpiod.LineSettings(direction=Direction.INPUT, bias=gpiod.line.Bias.PULL_UP)})
-time.sleep(0.001)
+dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN: gpiod.LineSettings(direction=Direction.OUTPUT,output_value=value)})
 dht_line.release()
 time.sleep(1.0)
 
@@ -23,64 +21,74 @@ time.sleep(1.0)
 
 def read_dht22_data():
 
-    # pulse the DHT22 pin  go low then high
-    CHIP = "/dev/gpiochip0"
-    chip = gpiod.Chip(CHIP)
-    value_str = {Value.ACTIVE: "Active", Value.INACTIVE: "Inactive"}
-    value = Value.ACTIVE
-
     data = []
+
+    # Configure the DHT pin for output
     dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN: gpiod.LineSettings(direction=Direction.OUTPUT,output_value=value)})
 
- 
-    time.sleep(1)
+    # Pulse the DHT pin for 2 millisec
     dht_line.set_value(DHT_PIN,Value.INACTIVE)
-    time.sleep(0.018)
+    time.sleep(0.002)
     dht_line.set_value(DHT_PIN,Value.ACTIVE)
-
     dht_line.release()
+
+    # Configure the  line for input  (Must be released from previous use)
     dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN:  gpiod.LineSettings(direction=Direction.INPUT, bias=gpiod.line.Bias.PULL_UP)})
 
-    start_time_1 = time.time()
+    if (False):
+        print (dht_line.get_value(DHT_PIN))
+        print (dht_line.get_value(DHT_PIN))
+        print (dht_line.get_value(DHT_PIN))
+        print (dht_line.get_value(DHT_PIN))
+        print (dht_line.get_value(DHT_PIN))
+        print (dht_line.get_value(DHT_PIN))
 
-    print (dht_line.get_value(DHT_PIN))
-    print (dht_line.get_value(DHT_PIN))
-    print (dht_line.get_value(DHT_PIN))
-    print (dht_line.get_value(DHT_PIN))
-    print (dht_line.get_value(DHT_PIN))
-    print (dht_line.get_value(DHT_PIN))
+        for index in range(500):
+            print ( int(dht_line.get_value(DHT_PIN) == Value.INACTIVE), end="" )
 
-    # start_time_2 = time.time()
-
-    for index in range(500):
-        print ( int(dht_line.get_value(DHT_PIN) == Value.INACTIVE), end="" )
-
-    print("")
+        print("")
 
 
     time_now = time.time()
 
     # Reconfigure as input
-    dht_line.release()
-    dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN:  gpiod.LineSettings(direction=Direction.INPUT, bias=gpiod.line.Bias.PULL_UP)})
+    # dht_line.release()
+    # dht_line = gpiod.request_lines( CHIP,  config={DHT_PIN:  gpiod.LineSettings(direction=Direction.INPUT, bias=gpiod.line.Bias.PULL_UP)})
 
     while True:
         current_time = time.time()
-        if current_time - time_now > 0.0002:
+        # if current_time - time_now > 0.0002:     # Stop waiting after 200 uSec - Original
+        if current_time - time_now > 0.004:     # Stop waiting after 200 uSec
             break
-        if (bool(dht_line.get_value(DHT_PIN)) == False):
+        # if (bool(dht_line.get_value(DHT_PIN)) == False):     original
+        if (bool(dht_line.get_value(DHT_PIN) == Value.INACTIVE)):  #  Original
+        # if (bool(dht_line.get_value(DHT_PIN) == Value.ACTIVE)):  Trying inverted data
             data.append(0)
         else:
             data.append(1)
     
     start = 0
     
+    print ("Len(data): ", len(data))
+
+    for counter in range(len(data)):
+        print(data[counter], end="")
+    print ("")
+    # input("Paused")
+
+    # find the first  transistion from Low  to High
     for i in range(len(data)):
         if data[i] == 0 and data[i+1] == 1:
             start = i + 2
             break
     
     data = data[start:]
+
+    print ("Len(data): ", len(data), "    Start: ", start)
+
+    for counter in range(len(data)):
+        print(data[counter], end="")
+    print ("")
 
     humidity_bits = data[0:8]
     humidity_decimal_bits = data[8:16]
